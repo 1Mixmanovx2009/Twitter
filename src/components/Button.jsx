9fbcc5fbd8af4347c9293438e772ae489d430349
1 Mixmanovx 2009 <xojixoji422@gmail.com>
2024-09-27 07:04:13 +0500

    qoshildi yangi loiha

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({title, type, exrtoStyle}) {
   return (
-    <button className={`p-[18px] font-bold text-[18px] leading-[23px] text-white bg-[#1DA1F2] w-full rounded-[76px] ${exrtoStyle}`} type={type}>{title}</button>
+    <button className={`p-[18px] font-bold text-[18px] leading-[23px] text-white bg-[#1DA1F2] w-full ${exrtoStyle}`} type={type}>{title}</button>
   )
 }
 
